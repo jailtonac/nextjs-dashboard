9bf712624a3d59f1dea6f63c752cbda7edb04702
Jailton A. Cordeiro <jailton.araujo@gmail.com>
2025-11-23 15:23:02 -0300

    Commit inicial.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { SUBRESOURCE_INTEGRITY_MANIFEST } from 'next/dist/shared/lib/constants';
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700']  });
+
